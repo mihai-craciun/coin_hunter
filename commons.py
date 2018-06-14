c49219bfa4a614314c29e9359d92a82fbb221868
Mihai Craciun <mihaicraciun96@gmail.com>
2018-06-14 19:00:44 +0300

    Working begin and end

--- commons.py
+++ commons.py
@@ -6,13 +6,14 @@ import numpy as np
 FPS = 30
 CAMERA_X = 0
 CAMERA_Y = 0
-SCALE = 4
-BLOCKS_X = 8
-BLOCKS_Y = 8
+SCALE = 3
+BLOCKS_X = 16
+BLOCKS_Y = 16
 B_SIZE = 16
 # define styles
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+DEEPGREY = (50, 50, 50)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
@@ -52,7 +53,7 @@ COLLECTIBLES = [
     BLUE_CRYSTAL,
     RED_CRYSTAL
 ]
-
+ITEMS = TERRAINS + COLLECTIBLES
 # initialize pygame and create window
 B_SIZE = B_SIZE * SCALE
 WIDTH = BLOCKS_X * B_SIZE
@@ -64,17 +65,24 @@ FONT = pygame.font.Font("pixelated.ttf", 8*SCALE)
 
 
 # Map
-class Map:
-    def __init__(self):
-        self.map = [[WATER for _ in range(HEIGHT//B_SIZE)]
-                    for _ in range(WIDTH//B_SIZE)]
+BLANK_SURFACE = pygame.Surface((B_SIZE, B_SIZE))
+BLANK_SURFACE.fill(DEEPGREY)
 
-    def draw(self, screen):
+
+class Map:
+    def __init__(self, rows=8, cols=4):
+        self.map = [[WATER for i in range(rows)]
+                    for j in range(cols)]
+        self.map[1][0] |= COIN
+
+    def draw(self, screen, cam_x=0, cam_y=0):
         for i, line in enumerate(self.map):
             for j, item in enumerate(line):
                 # Position
-                pos = (i*B_SIZE - CAMERA_Y, j*B_SIZE - CAMERA_X)
+                pos = (i*B_SIZE - cam_x, j*B_SIZE - cam_y)
                 # Check if has terrain to draw
+                if item == BLANK:
+                    screen.blit(BLANK_SURFACE, pos)
                 for t in TERRAINS:
                     if item & t > 0:
                         screen.blit(PNGS[t], pos)
@@ -82,4 +90,41 @@ class Map:
                 for c in COLLECTIBLES:
                     if item & c > 0:
                         screen.blit(PNGS[c], pos)
-        
+
+    def setbegin(self, x, y):
+        if x >= len(self.map):
+            return
+        if y >= len(self.map[0]):
+            return
+        if y < 0:
+            for i in range(len(self.map)):
+                self.map[i] = [BLANK for _ in range(-y)] + self.map[i]
+        if x < 0:
+            self.map =[[BLANK for a in self.map[0]] for b in range(-x)] + self.map
+        if y > 0:
+            for i in range(len(self.map)):
+                self.map[i] = self.map[i][y:]
+        if x > 0:
+            self.map = self.map[x:]
+
+    def setend(self, x, y):
+        if x < 0:
+            return
+        if y < 0:
+            return
+        if y+1 > len(self.map[0]):
+            for i in range(len(self.map)):
+                self.map[i] = self.map[i] + [BLANK for _ in range(y-len(self.map[i])+1)]
+        if x+1 > len(self.map):
+            self.map = self.map + [[BLANK for a in self.map[0]] for b in range(x-len(self.map)+1)]
+        if y+1 < len(self.map[0]):
+            for i in range(len(self.map)):
+                self.map[i] = self.map[i][:y+1]
+        if x+1 < len(self.map):
+            self.map = self.map[:x+1]
+    
+    def printsize(self):
+        print([len(self.map[i]) for i in range(len(self.map))])
+    
+    def printbock(self, x, y):
+        print(self.map[x][y])
--- mapeditor.py
+++ mapeditor.py
@@ -17,9 +17,28 @@ CRT_BLOCK.fill(WHITE)
 CRT_BLOCK_X = 0
 CRT_BLOCK_Y = 0
 
-TEXT_CRT = FONT.render("Current   block:", False, WHITE)
-TEXT_HELP_TOGGLE = FONT.render("help : H", False, WHITE)
+TEXT_CRT = FONT.render("Current block:", False, WHITE)
+TEXT_HELP_TOGGLE = FONT.render("Help : H", False, WHITE)
 
+TEXT_HELP_ITEMS = [
+    "Help: H",
+    "Set map begin: B",
+    "Set map finish: F",
+    "Delete crt: D",
+    "XOR crt: SPACE",
+    "Change item: Tab",
+    "Move: Arrows",
+]
+
+for i in range(len(TEXT_HELP_ITEMS)):
+    TEXT_HELP_ITEMS[i] = FONT.render(TEXT_HELP_ITEMS[i], False, WHITE)
+
+ITEM_CRT = WATER
+ITEM_CRT_SURFACE_OFFSET = 10
+ITEM_CRT_SURFACE = pygame.Surface(
+    (B_SIZE+2*ITEM_CRT_SURFACE_OFFSET, B_SIZE+2*ITEM_CRT_SURFACE_OFFSET))
+ITEM_CRT_SURFACE.fill(WHITE)
+ITEM_CRT_SURFACE.set_alpha(128)
 SHOW_HELP = False
 MOVE_TICKER = 3
 
@@ -33,38 +52,92 @@ while running:
         # check for closing the window
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_1:
+                game_map.printsize()
+            if event.key == pygame.K_2:
+                game_map.printbock(CRT_BLOCK_X, CRT_BLOCK_Y)
+            if event.key == pygame.K_TAB:
+                # Change current item
+                ITEM_CRT = ITEMS[(ITEMS.index(ITEM_CRT)+1) % len(ITEMS)]
+            if CRT_BLOCK_Y > -1 and CRT_BLOCK_Y < len(game_map.map[0]) and CRT_BLOCK_X > -1 and CRT_BLOCK_X < len(game_map.map):
+                # Only on map
+                if event.key == pygame.K_SPACE:
+                    # Changing item type with existing
+                    game_map.map[CRT_BLOCK_X][CRT_BLOCK_Y] ^= ITEM_CRT
+                if event.key == pygame.K_d:
+                    game_map.map[CRT_BLOCK_X][CRT_BLOCK_Y] = BLANK
+            if event.key == pygame.K_b:
+                game_map.setbegin(CRT_BLOCK_X, CRT_BLOCK_Y)
+                if CRT_BLOCK_X < 0:
+                    CAMERA_X -= B_SIZE * CRT_BLOCK_X
+                    CRT_BLOCK_X = 0
+                if CRT_BLOCK_Y < 0:
+                    CAMERA_Y -= B_SIZE * CRT_BLOCK_Y
+                    CRT_BLOCK_Y = 0
+                if CRT_BLOCK_X > 0:
+                    CAMERA_X -= B_SIZE * CRT_BLOCK_X
+                    CRT_BLOCK_X = 0
+                if CRT_BLOCK_Y > 0:
+                    CAMERA_Y -= B_SIZE * CRT_BLOCK_Y
+                    CRT_BLOCK_Y = 0
+            if event.key == pygame.K_e:
+                game_map.setend(CRT_BLOCK_X, CRT_BLOCK_Y)
+                if CRT_BLOCK_X >= len(game_map.map):
+                    CRT_BLOCK_X = len(game_map.map)-1
+                if CRT_BLOCK_Y < 0:
+                    CRT_BLOCK_Y = 0
+                if CRT_BLOCK_X <= len(game_map.map):
+                    CRT_BLOCK_X = len(game_map.map)-1
+                if CRT_BLOCK_Y > 0:
+                    CRT_BLOCK_Y > 0
 
     # Update
     all_sprites.update()
     # Update texts
     TEXT_Y = FONT.render("Y:{}".format(CRT_BLOCK_Y), False, WHITE)
     TEXT_X = FONT.render("X:{}".format(CRT_BLOCK_X), False, WHITE)
+    TEXT_SIZE = FONT.render("Size:{0}x{1}".format(len(game_map.map), len(game_map.map[0])), False, WHITE)
     # Get input
     keys = pygame.key.get_pressed()
-    if MOVE_TICKER == 0:
+    if MOVE_TICKER == 0 and set(keys) != set([0]):
         MOVE_TICKER = 3
         if keys[pygame.K_RIGHT]:
             CRT_BLOCK_X += 1
+            if CRT_BLOCK_X*B_SIZE - CAMERA_X >= WIDTH:
+                CAMERA_X += B_SIZE
         if keys[pygame.K_LEFT]:
             CRT_BLOCK_X -= 1
+            if CRT_BLOCK_X*B_SIZE - CAMERA_X < 0:
+                CAMERA_X -= B_SIZE
         if keys[pygame.K_DOWN]:
             CRT_BLOCK_Y += 1
+            if CRT_BLOCK_Y*B_SIZE - CAMERA_Y >= HEIGHT:
+                CAMERA_Y += B_SIZE
         if keys[pygame.K_UP]:
             CRT_BLOCK_Y -= 1
+            if CRT_BLOCK_Y*B_SIZE - CAMERA_Y < 0:
+                CAMERA_Y -= B_SIZE
     if MOVE_TICKER > 0:
         MOVE_TICKER -= 1
     # Draw / Render
     screen.fill(BLACK)
     # Draw map
-    game_map.draw(screen)
+    game_map.draw(screen, cam_x=CAMERA_X, cam_y=CAMERA_Y)
     # Draw blink
-    screen.blit(CRT_BLOCK, (CRT_BLOCK_X*B_SIZE, CRT_BLOCK_Y*B_SIZE))
+    screen.blit(CRT_BLOCK, (CRT_BLOCK_X*B_SIZE -
+                            CAMERA_X, CRT_BLOCK_Y*B_SIZE - CAMERA_Y))
     # Draw stats
     screen.blit(TEXT_HELP_TOGGLE, (WIDTH - TEXT_HELP_TOGGLE.get_width(),
                                    HEIGHT - TEXT_HELP_TOGGLE.get_height()))
-    screen.blit(TEXT_X, (10, HEIGHT - TEXT_X.get_height()*2))
-    screen.blit(TEXT_Y, (10, HEIGHT - TEXT_X.get_height()))
+    screen.blit(TEXT_X, (10, HEIGHT - TEXT_X.get_height()*3))
+    screen.blit(TEXT_Y, (10, HEIGHT - TEXT_X.get_height()*2))
+    screen.blit(TEXT_SIZE, (10, HEIGHT - TEXT_X.get_height()))
     screen.blit(TEXT_CRT, (WIDTH - TEXT_CRT.get_width(), 0))
+    screen.blit(ITEM_CRT_SURFACE, (WIDTH -
+                                   ITEM_CRT_SURFACE.get_width() - 10, TEXT_CRT.get_height()))
+    screen.blit(PNGS[ITEM_CRT], (WIDTH - ITEM_CRT_SURFACE.get_width() -
+                                 10 + 10, TEXT_CRT.get_height()+10))
     all_sprites.draw(screen)
     # *after* drawing everything
     pygame.display.flip()
